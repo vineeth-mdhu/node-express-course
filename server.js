0e6f37745cb6e033d1b776eef9f7707377c5ff83
Vineeth Madhu <vineethnbr13@gmail.com>
2021-08-28 18:42:45 +0530

    add post route

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
+
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
 ]
+
+app.use(bodyParser.json);
+
+app.post('/login',(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="username";
+    const mockPassword="password";
+
+    if(username==mockUsername && password==mockPassword){
+        res.json({
+            success:true,
+            message:'password and username match!',
+            token:'encrypted token goes here'
+        })
+    }else{
+        res.json({
+            success:false,
+            message:'password and username do not match'
+        })
+    }
+})
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
 
 app.get('/users/:id',(req,res)=>{
     console.log(req.params.id)
